11a9526f1be948fc736c3b59832c8693628d8692
VanamaTharunKumar <tharunkumarvanama@gmail.com>
2026-01-20 10:02:39 +0000

    commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World! This is my Node.js app running on Windows.');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
